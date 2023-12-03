2839eb0ca396dfecadd8d566d800279e2e38c79a
pelusa-v <razor0164@gmail.com>
2023-12-03 01:02:16 -0500

    Notes controller done

--- claim-based-auth-sample.API/Controllers/NotesController.cs
+++ claim-based-auth-sample.API/Controllers/NotesController.cs
@@ -2,14 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using claim_based_auth_sample.Application;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace claim_based_auth_sample.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class NotesController : ControllerBase
     {
-        
+        private readonly INotesService _notesService;
+        public NotesController(INotesService notesService)
+        {
+            _notesService = notesService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<NoteDTO>>> ListNotes()
+        {
+            return await _notesService.ListNotes();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<NoteDTO>> CreateNote(CreateNoteDTO dto)
+        {
+            return await _notesService.CreateNote(dto, "");
+        }
     }
 }
---
+++ claim-based-auth-sample.Application/DTOs/CreateNoteDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace claim_based_auth_sample.Application;
+
+public class CreateNoteDTO
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Content { get; set; }
+}
